d9796a2b3ebbde6f3ef398268d7f696ca8f8e7cb
woodxzw <85925484+woodxzw@users.noreply.github.com>
2023-08-16 16:48:38 +0800

    commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# Notion Custom Domain
+
+Custom domains for your public Notion pages. You can publish your page to your own domain instead of `notion.site`.
+
+[![Notion Custom Domain](https://user-images.githubusercontent.com/19500280/93695277-d99aa400-fb4f-11ea-8e82-5c431110ce19.png)](https://notion-custom-domain.hosso.co)
+
+## Getting Started
+
+Install dependencies:
+
+```
+yarn
+```
+
+Then deploy to Vercel with specifiying your public Notion page:
+
+```
+PAGE_URL=https://<your-domain>.notion.site/<Your-Page-ID> \
+yarn deploy:prod
+```
+
+For example:
+
+```
+PAGE_URL=https://notion.notion.site/Notion-Official-83715d7703ee4b8699b5e659a4712dd8 \
+yarn deploy:prod
+```
+
+Finally, set up a custom domain for the deployment on the Vercel Dashboard. See [Custom Domains – Vercel Docs](https://vercel.com/docs/concepts/projects/custom-domains)
+
+![](https://user-images.githubusercontent.com/19500280/169642461-c31df143-a8a5-4d37-8494-e5b04b01c7b1.png)
+
+## Development
+
+### Run locally with `vercel dev`
+
+```
+PAGE_URL=https://<your-domain>.notion.site/<Your-Page-ID> \
+yarn develop
+```
+
+Then open http://localhost:3000.
+
+### Debug with Node Inspector
+
+```
+yarn debug
+```
+
+Then open http://localhost:3000.
+
+## Google Analytics Support
+
+Deploying with `GA_TRACKING_ID` environment variable injects the tracking code into your public Notion page:
+
+```
+PAGE_URL=https://<your-domain>.notion.site/<Your-Page-ID> \
+GA_TRACKING_ID=UA-XXXXXXXXX-X \
+yarn deploy:prod
+```
+
+## Using Environment Variables on the Vercel Dashboard
+
+You can use environment variables on the Vercel Dashboard. In this case, you can simply run
+`yarn develop`, `yarn deploy` or `yarn deploy:prod` without setting environment variables.
+![](https://user-images.githubusercontent.com/19500280/169643081-9e5e1f72-dacb-4892-b9d6-f67869c2a34d.png)
+
+## License
+
+[MIT](LICENSE)
---
+++ src/index.ts
@@ -0,0 +1,127 @@
+import express from 'express';
+import proxy from 'express-http-proxy';
+import { URL } from 'url';
+import path from 'path';
+
+const {
+  PAGE_URL = 'https://notion.notion.site/Notion-Official-83715d7703ee4b8699b5e659a4712dd8',
+  GA_TRACKING_ID,
+} = process.env;
+
+const { origin: pageDomain, pathname: pagePath } = new URL(PAGE_URL);
+const pageId = path.basename(pagePath).match(/[^-]*$/);
+
+// Map start page path to "/". Replacing URL for example:
+// - https://my.notion.site/0123456789abcdef0123456789abcdef -> https://mydomain.com/
+// - /My-Page-0123456789abcdef0123456789abcdef -> /
+// - /my/My-Page-0123456789abcdef0123456789abcdef -> /
+const ncd = `var ncd={
+  href:function(){return location.href.replace(location.origin,"${pageDomain}").replace(/\\/(?=\\?|$)/,"/${pageId}")},
+  pushState:function(a,b,url){history.pushState(a,b,url.replace("${pageDomain}",location.origin).replace(/(^|[^/])\\/[^/].*${pageId}(?=\\?|$)/,"$1/"));pageview();},
+  replaceState:function(a,b,url){history.replaceState(a,b,url.replace("${pageDomain}",location.origin).replace(/(^|[^/])\\/[^/].*${pageId}(?=\\?|$)/,"$1/"));pageview();}
+};`.replace(/\n */gm, '');
+
+const ga = GA_TRACKING_ID
+  ? `<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '${GA_TRACKING_ID}');
+</script>`
+  : '';
+
+const pageview = `<script>
+  window.pagePath = location.pathname + location.search + location.hash;
+  function pageview(){
+    var pagePath = location.pathname + location.search + location.hash;
+    if (pagePath !== window.pagePath) {${
+      GA_TRACKING_ID
+        ? `
+      gtag('config', '${GA_TRACKING_ID}', {'page_path': pagePath});`
+        : ''
+    }
+      window.pagePath = pagePath;
+    }
+  }
+  window.addEventListener('popstate', pageview);
+</script>`;
+
+const app = express();
+
+app.use(
+  proxy(pageDomain, {
+    proxyReqOptDecorator: (proxyReqOpts) => {
+      if (proxyReqOpts.headers) {
+        proxyReqOpts.headers['accept-encoding'] = 'gzip';
+      }
+      return proxyReqOpts;
+    },
+    proxyReqPathResolver: (req) => {
+      // Replace '/' with `/${pageId}`
+      return req.url.replace(/\/(\?|$)/, `/${pageId}$1`);
+    },
+    userResHeaderDecorator: (headers, userReq) => {
+      if (headers['location']) {
+        // "https://www.notion.so/syncCookies?backUrl=https%3A%2F%2Fmy.notion.site%2F0123456789abcdef0123456789abcdef%3Fv%3D1"
+        // -> "https://mydomain.com/syncCookies?backUrl=https%3A%2F%2Fmydomain.com%2F0123456789abcdef0123456789abcdef%3Fv%3D1"
+        headers['location'] = headers['location'].replace(
+          /(https?)(:\/\/|%3A%2F%2F)[^.]+\.notion\.(so|site)/g,
+          `${userReq.headers['x-forwarded-proto']}$2${userReq.headers['x-forwarded-host']}`,
+        );
+      }
+
+      if (headers['set-cookie']) {
+        // "Domain=notion.site" -> "Domain=mydomain.com"
+        // "; Domain=notion.site;' -> '; Domain=mydomain.com;"
+        const domain = (userReq.headers['x-forwarded-host'] as string).replace(
+          /:.*/,
+          '',
+        );
+        headers['set-cookie'] = headers['set-cookie'].map((cookie) =>
+          cookie.replace(
+            /((?:^|; )Domain=)((?:[^.]+\.)?notion\.(?:so|site))(;|$)/g,
+            `$1${domain}$3`,
+          ),
+        );
+      }
+
+      const csp = headers['content-security-policy'] as string;
+      if (csp) {
+        headers['content-security-policy'] = csp.replace(
+          /(?=(script-src|connect-src) )[^;]*/g,
+          '$& https://www.googletagmanager.com https://www.google-analytics.com',
+        );
+      }
+
+      return headers;
+    },
+    userResDecorator: (_proxyRes, proxyResData, userReq) => {
+      if (/^\/app-.*\.js$/.test(userReq.url)) {
+        return proxyResData
+          .toString()
+          .replace(/^/, ncd)
+          .replace(/window.location.href(?=[^=]|={2,})/g, 'ncd.href()') // Exclude 'window.locaton.href=' but not 'window.locaton.href=='
+          .replace(/window.history.(pushState|replaceState)/g, 'ncd.$1');
+      } else if (/^\/image[s]?\//.test(userReq.url)) {
+        return proxyResData;
+      } else {
+        // Assume HTML
+        return proxyResData
+          .toString()
+          .replace('</body>', `${ga}${pageview}</body>`);
+      }
+    },
+  }),
+);
+
+if (!process.env.VERCEL_REGION) {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () =>
+    console.log(`Server running at http://localhost:${port}`),
+  );
+}
+
+export default app;
